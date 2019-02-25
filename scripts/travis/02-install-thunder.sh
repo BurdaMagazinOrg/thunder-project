f4925df6b2c5a1ff5205854f783857753dcd6091
Daniel Bosen <daniel.bosen@burda.com>
2019-02-25 17:07:27 +0100

    Remove drupal scaffold (#30)

--- scripts/travis/02-install-thunder.sh
+++ scripts/travis/02-install-thunder.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 composer install
-composer drupal-scaffold
 
 cd docroot
 /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush si thunder --db-url=mysql://travis@127.0.0.1/thunder -y
